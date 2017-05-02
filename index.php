9f9514b0a18194973613f56adf5eee075014ad04
crisjsg <dawcrisjsg@gmail.com>
2017-05-02 17:50:15 +0200

    Añadidas validaciones en el primer formulario

--- index.php
+++ index.php
@@ -29,64 +29,72 @@ and open the template in the editor.
         <div id="formulario">
             <h3>Formulario</h3>
             <form name="formulario_datos_paquetes" method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-                Ancho:
-                <input type="text" name="ancho" placeholder="Anchura del paquete (cm)" />
-                <br>
-                Alto:
-                <input type="text" name="alto" placeholder="Altura del paquete (cm)" />
-                <br>
-                Profundo:
-                <input type="text" name="profundo" placeholder="Profundidad del paquete (cm)" />
-                <br>
-                Peso:
-                <input type="text" name="peso" placeholder="Peso del paquete (kg)" />
-                <br>
-                Corta descripción del paquete:
-                <textarea name="descripcion_paquete" rows="2" cols="50" maxlength="50" placeholder="Maximo 50 caracteres"></textarea>
 
+                <br>
+                <label>Ancho*:
+                    <input type="number" name="ancho" placeholder="Anchura del paquete (cm)" min="1" max="300" oninvalid="setCustomValidity(errorAnchura)" oninput="setCustomValidity('')"  required/>
+                </label>
+                <br>
+                <label>Alto*:
+                    <input type="number" name="alto" placeholder="Altura del paquete (cm)" min="1" max="300" oninvalid="setCustomValidity(errorAltura)" oninput="setCustomValidity('')" required/>
+                </label>
+                <br>
+                <label>Profundo*:
+                    <input type="number" name="profundo" placeholder="Profundidad del paquete (cm)" min="1" max="300" oninvalid="setCustomValidity(errorProfundidad)" oninput="setCustomValidity('')" required/>
+                </label>
+                <br>
+                <label>Peso*:
+                    <input type="number" name="peso" placeholder="Peso del paquete (kg)" min="1" max="300" oninvalid="setCustomValidity(errorPeso)" oninput="setCustomValidity('')" required/>
+                </label>
+                <br>
+                <label>Corta descripción del contenido del paquete:
+                    <br>
+                    <textarea name="descripcion_paquete" rows="2" cols="50" maxlength="50" placeholder="Maximo 50 caracteres"></textarea>
+                </label>
                 <br>
                 <input type="submit" value="Añadir paquete" name="paquetes" />
             </form>
 
             <?php
             //Si hay datos en la variable 'paquetes' en el envio por POST haz...
-            if (isset($_POST['paquetes'])) {
-                if (isset($_POST['paquetes']) && ($_POST['paquetes'] != "")) {
-                    ?>
-                    <div id="envio">
-                        <h3>Paquetes</h3>
-                        <?php
-                        /*
-                         * $_POST['paquetes'] -> Contiene los paquetes que posteriormente se guardaran en 
-                         * $_SESSION['comparar_paquetes'] para que luego se realice las peticiones a la API
-                         */
-                        /**
-                         * La idea es que se envien varios paquetes por POST y luego ir guardando en la variable $_SESSION['comparar_paquetes'] cada paquete
-                         * por individual para ir añadiendo de uno en uno con sus tamaños en las peticiones que se harán.
-                         */
-                        //Datos recibidos del formulario
-                        $ancho = filter_input(INPUT_POST, 'ancho');
-                        $alto = filter_input(INPUT_POST, 'alto');
-                        $profundo = filter_input(INPUT_POST, 'profundo');
-                        $peso = filter_input(INPUT_POST, 'peso');
-                        $descripcion = filter_input(INPUT_POST, 'descripcion_paquete');
-                        //Creamos Objeto Paquete
-                        $paquete = new Paquete($ancho, $alto, $profundo, $peso, $descripcion);
-                        //Guardamos el array de paquetes una vez tengamos todos los paquetes en la variable sesion
-                        $_SESSION['ses_paquetes'][] = $paquete;
+
+            if (isset($_POST['paquetes']) && ($_POST['paquetes'] != "")) {
+                ?>
+                <div id="envio">
+                    <h3>Paquetes</h3>
+                    <?php
+                    /*
+                     * $_POST['paquetes'] -> Contiene los paquetes que posteriormente se guardaran en 
+                     * $_SESSION['comparar_paquetes'] para que luego se realice las peticiones a la API
+                     */
+                    /**
+                     * La idea es que se envien varios paquetes por POST y luego ir guardando en la variable $_SESSION['comparar_paquetes'] cada paquete
+                     * por individual para ir añadiendo de uno en uno con sus tamaños en las peticiones que se harán.
+                     */
+                    //Datos recibidos del formulario
+                    $ancho = filter_input(INPUT_POST, 'ancho');
+                    $alto = filter_input(INPUT_POST, 'alto');
+                    $profundo = filter_input(INPUT_POST, 'profundo');
+                    $peso = filter_input(INPUT_POST, 'peso');
+                    $descripcion = filter_input(INPUT_POST, 'descripcion_paquete');
+                    //Creamos Objeto Paquete
+                    $paquete = new Paquete($ancho, $alto, $profundo, $peso, $descripcion);
+                    //Guardamos el array de paquetes una vez tengamos todos los paquetes en la variable sesion
+                    $_SESSION['ses_paquetes'][] = $paquete;
 
 
-                        for ($i = 0; $i < count($_SESSION['ses_paquetes']); $i++) {
-                            $medidas_paquete = $_SESSION['ses_paquetes'][$i]->mostrar_medidas_paquete();
-                            echo '<div>'
-                            . '<span>'
-                            . $medidas_paquete .
-                            '</span><span id="numero_paquete">' . $i . '</span><button onclick="quitarPaquete(this)">X</button>'
-                            . '</div>';
-                        }
-                        //var_dump($_SESSION);
-                        //session_destroy();
+                    for ($i = 0; $i < count($_SESSION['ses_paquetes']); $i++) {
+                        $medidas_paquete = $_SESSION['ses_paquetes'][$i]->mostrar_medidas_paquete();
+                        echo '<div>'
+                        . '<span>'
+                        . $medidas_paquete .
+                        '</span><span id="numero_paquete">' . $i . '</span><button onclick="quitarPaquete(this)">X</button>'
+                        . '</div>';
                     }
+                }else{
+                    $_POST['paquetes'] = NULL;
+                    session_destroy();
+                    //$_SESSION['ses_paquetes'] = NULL;
                 }
                 ?>
             </div>
--- js/formulario.js
+++ js/formulario.js
@@ -2,6 +2,26 @@
  * Aquí si incluye todo el código JavaScript que interactuará con los formularios
  */
 
+
+
+
+/**
+ * Variables que se utilicen para el formulario
+ */
+
+    // Variables con los mensajes de error
+    var errorAnchura = "No has introducido una anchura valida";
+    var errorAltura  = "No has introducido una altura";
+    var errorProfundidad  = "No has introducido una profundidad valida";
+    var errorPeso  = "No has introducido un peso valido";
+
+
+/**
+ * 
+ * Métodos que interactuan con los formularios
+ *
+ */
+
 var quitarPaquete = function (elemento) {
     var paquete = elemento.previousSibling.textContent;
     //Para quitar el paquete de la lista
